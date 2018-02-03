6f04b9f8fb42e05deb18f5750a5f571399159c86
camilaibs <camilaibs@gmail.com>
2018-02-03 13:01:42 -0200

    teste

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-import SecaoPostit from './secaoPostit.js';
+import App from './components/app/app.js';
 
-let tag = document.getElementsByClassName("notes")[0];
-const secaoPostit = new SecaoPostit(tag);
-
-window.editaFormulario = posicao => secaoPostit.edita(posicao);
-
-window.adicionarNota = (formulario, inputTitulo, textareaTexto, posicao) => {
-    let titulo = inputTitulo.value, texto = textareaTexto.value;
-    
-    if (posicao !== null && posicao > -1) {
-        secaoPostit.salva(posicao, titulo, texto);
-    } else {
-        secaoPostit.adiciona(titulo, texto);
-        formulario.reset();
-    }
-}
-
-window.removerNota = (evento, posicao) => {
-    evento.stopPropagation();
-    secaoPostit.remove(posicao);
-}
+let body = document.body;
+body.insertBefore(
+    new App(), 
+    body.firstChild
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'lib')
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
